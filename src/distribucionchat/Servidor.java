ba7ea9af0300f7b0b8f1a8cb53e480d7ea0e58e7
RGaldamez <ricardo.j.galdamez@gmail.com>
2018-07-01 14:43:23 -0600

    rezando para server funcional

--- src/distribucionchat/Servidor.java
+++ src/distribucionchat/Servidor.java
@@ -11,7 +11,6 @@ import java.rmi.registry.Registry;
  *
  * @author Inti Velasquez
  */
-
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -20,17 +19,23 @@ import java.rmi.registry.Registry;
  * @author Inti Velasquez
  */
 public class Servidor {
-     
-    private void iniciarServidor(){
+
+    private void iniciarServidor() {
+
+        System.setProperty("java.security.policy", "./test.policy");
+        //System.setProperty("java.rmi.server.hostname", "190.53.65.153");
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
         try {
             // Crear el repositorio en el puerto 1099
             Registry registry = LocateRegistry.createRegistry(1099);
-             
+
             // Crea un nuevo servicio y lo registra en el repositorio
             registry.rebind("miMensaje", new MensajeImpl());
         } catch (Exception e) {
             e.printStackTrace();
-        }      
+        }
         System.out.println("El sistema esta listo");
     }
 
@@ -39,5 +44,4 @@ public class Servidor {
         servidor.iniciarServidor();
     }
 
-}   
-
+}
