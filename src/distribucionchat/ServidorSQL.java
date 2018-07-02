f3240fb56558ac07d09ecf3771a0dff8c35865b5
RGaldamez <ricardo.j.galdamez@gmail.com>
2018-07-02 14:49:29 -0600

    Funciones de la parte SQL

--- src/distribucionchat/ServidorSQL.java
+++ src/distribucionchat/ServidorSQL.java
@@ -8,14 +8,17 @@ package distribucionchat;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 public class ServidorSQL {
 
     public static void main(String[] args) {
         ServidorSQL server = new ServidorSQL();
-        server.Check_Data();
+        //server.Check_Data();
     }
 
     public void Check_Data() {
@@ -23,74 +26,181 @@ public class ServidorSQL {
         Connection connection = obj_DB_Connection.get_connection();
         PreparedStatement ps = null;
         try {
-            String query = "select * from test1";
+            String query = "select FirstName,LastName from test2 where FirstName='Mario'";
             ps = connection.prepareStatement(query);
             ps.execute();
             ResultSet rs = ps.getResultSet();
             while (rs.next()) {
-                System.out.println("name- " + rs.getString("name"));
+                System.out.println("FirstName - " + rs.getString("FirstName"));
+                System.out.println("LastName - " + rs.getString("LastName"));
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println(e);
         }
     }
-    
-    public int createUser(String nombreCompleto, String loginName, String password){
-        /*
-            uuid
-            nombre completo
-            login name
-            password
-            fecha de creacion
-            fecha de accesos al sistema
-        
-        */
+
+    public void createUser(String nombreCompleto, String loginName, String password) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/dd");
+        String date = sdf.format(new Date());
+        UUID uuid = UUID.randomUUID();
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "insert into Usuarios Values ('" + uuid + "', '" + nombreCompleto + "', '" + loginName + "', '" + password + "', " + date + ")";
+            ps = connection.prepareStatement(query);
+            ps.executeUpdate();
+            System.out.println("Usuario agregado con exito");
+
+        } catch (SQLException e) {
+            System.err.println("Usuario no agregado");
+        }
+
+    }
+
+    public String loginRequest(String loginName, String password) {
+        String temp = "Invalid User";
+
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "select UUID from Usuarios where LoginName='" + loginName + "' and Password='" + password + "'";
+            ps = connection.prepareStatement(query);
+            ps.execute();
+            ResultSet rs = ps.getResultSet();
+            temp = rs.getString("UUID");
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return temp;
+    }
+
+    public ArrayList<String> lookupUser(UUID uuid) {
+        ArrayList<String> temp = new ArrayList();
+
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "select UUID,NombreCompleto,LoginName,FechaCreacion from Usuarios where UUID='" + uuid + "'";
+            ps = connection.prepareStatement(query);
+            ps.execute();
+            ResultSet rs = ps.getResultSet();
+
+            temp.add(rs.getString("UUID"));
+            temp.add(rs.getString("NombreCompleto"));
+            temp.add(rs.getString("LoginName"));
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/dd");
+            String date = sdf.format(rs.getDate("FechaCreacion"));
+
+            temp.add(date);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return temp;
+
+    }
+
+    public ArrayList<String> lookupUser(String username) {
+        ArrayList<String> temp = new ArrayList();
+
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "select UUID,NombreCompleto,LoginName,FechaCreacion from Usuarios where LoginName='" + username + "'";
+            ps = connection.prepareStatement(query);
+            ps.execute();
+            ResultSet rs = ps.getResultSet();
+
+            temp.add(rs.getString("UUID"));
+            temp.add(rs.getString("NombreCompleto"));
+            temp.add(rs.getString("LoginName"));
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/dd");
+            String date = sdf.format(rs.getDate("FechaCreacion"));
+
+            temp.add(date);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return temp;
+
+    }
+
+    public int deleteUser(String username, String password) {
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "delete from Usuarios where LoginName='"+username+"'";
+            ps = connection.prepareStatement(query);
+            ps.executeUpdate();
+            System.out.println("Usuario Eliminado con exito");
+        } catch (SQLException e) {
+            System.err.println("Usuario No Eliminado");
+        }
+        return -1;
+    }
+
+    public int updateLoginUser(String newloginName, String oldLoginName) {
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "update Usuarios set LoginName='"+newloginName+"' where LoginName='"+oldLoginName+"'";
+            ps = connection.prepareStatement(query);
+            ps.executeUpdate();
+            System.out.println("Cambio realizado con exito");
+        } catch (SQLException e) {
+            System.err.println("No se logro realizar el cambio");
+        }
         
         return -1;
-      
     }
-    
-    public UUID loginRequest(String loginName, String password){
-        UUID temp = UUID.randomUUID();
-        
-        
+
+    public ArrayList<String> getUsers() {
+
+        ArrayList<String> temp = new ArrayList();
+        String user = "";
+
+        DB_Connection.DB_Connection obj_DB_Connection = new DB_Connection.DB_Connection();
+        Connection connection = obj_DB_Connection.get_connection();
+        PreparedStatement ps = null;
+        try {
+            String query = "select UUID,NombreCompleto,LoginName,FechaCreacion from Usuarios";
+            ps = connection.prepareStatement(query);
+            ps.execute();
+            ResultSet rs = ps.getResultSet();
+            while (rs.next()) {
+
+                temp.add(rs.getString("UUID"));
+                temp.add(rs.getString("NombreCompleto"));
+                temp.add(rs.getString("LoginName"));
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/dd");
+                String date = sdf.format(rs.getDate("FechaCreacion"));
+                temp.add(date);
+
+                System.out.println("UUID -> " + rs.getString("UUID"));
+                System.out.println("NombreCompleto-> " + rs.getString("NombreCompleto"));
+                System.out.println("LoginName -> " + rs.getString("LoginName"));
+                System.out.println("Fecha de Creacion -> " + rs.getDate("FechaCreacion"));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
         return temp;
     }
-    
-    public ArrayList<String> lookupUser(UUID uuid){
-        ArrayList temp = new ArrayList();
-        
-        
-        return temp;
-        
-    }
-    
-    public ArrayList<String> lookupUser(String username){
-        ArrayList temp = new ArrayList();
-        
-        
-        return temp;
-        
-    }
-    
-    public int deleteUser(String username, String password){
-        
-        
-        return -1;
-    }
-    
-    
-    public int updateLoginUser(String newloginName, String oldLoginName){
-        
-        return -1;
-    }
-    
-    public ArrayList<ArrayList<String>> getUsers (){
-        ArrayList<ArrayList<String>> temp = new ArrayList();
-        
-        
-        return temp;
-    }
-    
-    
+
 }
